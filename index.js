3faf950371a99e2eff25b17342b6b96b06873342
Oğuzhan Kalkuz <oguzhankalkuz@anayurtteknoloji.com>
2023-01-27 13:49:05 +0300

    removed dotenv, created configuration

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const api = process.env.FETCH_HTTP_ADDRESS || 'http://localhost:3001/api';
-
-const GetToken = () => localStorage.getItem('token');
+const config = {
+  api: '',
+  getAuthorization: () => '',
+};
 
 function HeaderBuilder(header, body) {
   const headers = {
     ...{ header },
     ...(body ? { 'Content-Type': 'application/json' } : null),
-    ...(GetToken() ? { authorization: `${GetToken()}` } : null),
+    ...(config.getAuthorization() ? { authorization: config.getAuthorization() } : null),
   };
 
   return headers;
@@ -27,36 +24,43 @@ async function Handler(res) {
 }
 
 export async function Get(endpoint, header = null, optionalApiAddress = null) {
-  const address = `${optionalApiAddress || api}${endpoint}`;
+  const address = `${optionalApiAddress || config.api}${endpoint}`;
 
   return fetch(address, { method: 'GET', headers: HeaderBuilder(header) })
     .then(Handler);
 }
 
 export async function Post(endpoint, body, header = null, optionalApiAddress = null) {
-  const address = `${optionalApiAddress || api}${endpoint}`;
+  const address = `${optionalApiAddress || config.api}${endpoint}`;
 
   return fetch(address, { method: 'POST', body: JSON.stringify(body), headers: HeaderBuilder(header, body) })
     .then(Handler);
 }
 
 export async function Patch(endpoint, body, header = null, optionalApiAddress = null) {
-  const address = `${optionalApiAddress || api}${endpoint}`;
+  const address = `${optionalApiAddress || config.api}${endpoint}`;
 
   return fetch(address, { method: 'PATCH', body: JSON.stringify(body), headers: HeaderBuilder(header, body) })
     .then(Handler);
 }
 
 export async function Put(endpoint, body, header = null, optionalApiAddress = null) {
-  const address = `${optionalApiAddress || api}${endpoint}`;
+  const address = `${optionalApiAddress || config.api}${endpoint}`;
 
   return fetch(address, { method: 'PUT', body: JSON.stringify(body), headers: HeaderBuilder(header, body) })
     .then(Handler);
 }
 
 export async function Delete(endpoint, body, header = null, optionalApiAddress = null) {
-  const address = `${optionalApiAddress || api}${endpoint}`;
+  const address = `${optionalApiAddress || config.api}${endpoint}`;
 
   return fetch(address, { method: 'DELETE', body: JSON.stringify(body), headers: HeaderBuilder(header, body) })
     .then(Handler);
 }
+
+function Configurate(api = '', getAuthorization = () => '') {
+  config.api = api;
+  config.getAuthorization = getAuthorization;
+}
+
+export default { Configurate };
